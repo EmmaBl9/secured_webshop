0d324963dfcfdd598c316383d117178b462956a4
EmmaBlanchoud <156648946+EmmaBl9@users.noreply.github.com>
2025-01-20 14:43:20 +0100

    feat(secured_webshop): lien https et création des routes login et register

--- app/server.js
+++ app/server.js
@@ -1,15 +1,30 @@
 const express = require("express");
-
+const fs = require("fs");
+const https = require("https");
 
 const app = express();
-const userRoute = require('./routes/User');
-app.use('/user', userRoute);
+//route pour user
+const userRoute = require("./routes/User");
+app.use("/user", userRoute);
 
+//route pour le login
+const loginRoute = require("./routes/Login");
+app.use("/Login", loginRoute);
 
+//route pour le register
+const registerRoute = require("./routes/Register");
+app.use("/Register", registerRoute);
+
+// Ajouter la licence
+// Charger les clés SSL
+const privateKey = fs.readFileSync("privkey.key", "utf8");
+const certificate = fs.readFileSync("certificate.crt", "utf8");
+const credentials = { key: privateKey, cert: certificate };
+
+// Créer le serveur HTTPS
+const httpsServer = https.createServer(credentials, app);
 
 // Démarrage du serveur
-app.listen(443, () => {
-    console.log('Server running on port 443');
+httpsServer.listen(443, () => {
+  console.log("Server running on port 443 with https");
 });
-
-//ajouter la licence, app.get...
