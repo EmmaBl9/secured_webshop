1f086840256c1afc2e92d411952ba84a2dd2264a
EmmaBlanchoud <156648946+EmmaBl9@users.noreply.github.com>
2025-01-27 14:42:24 +0100

    feat(Secured_webshop): afficher la page de login et de register

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,5 +1,14 @@
 module.exports = {
-    get: (req, res) => {
-        res.send("User: Sarah Test");
-    }
+  get: (req, res) => {
+    //Afficher l'html
+    res.sendFile("user.html", { root: "./view" });
+  },
+  login: (req, res) => {
+    //Afficher l'html
+    res.sendFile("login.html", { root: "./view" });
+  },
+  register: (req, res) => {
+    //Afficher l'html
+    res.sendFile("register.html", { root: "./view" });
+  },
 };
--- app/server.js
+++ app/server.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const fs = require("fs");
 const https = require("https");
-
 const app = express();
-//route pour user
+
+//Route pour user
 const userRoute = require("./routes/User");
 app.use("/user", userRoute);
-
-//route pour le login
-const loginRoute = require("./routes/Login");
-app.use("/Login", loginRoute);
-
-//route pour le register
-const registerRoute = require("./routes/Register");
-app.use("/Register", registerRoute);
 
 // Ajouter la licence
 // Charger les clés SSL
@@ -26,5 +18,5 @@ const httpsServer = https.createServer(credentials, app);
 
 // Démarrage du serveur
 httpsServer.listen(443, () => {
-  console.log("Server running on port 443 with https");
+  console.log("Server running on port https://localhost:443/user");
 });
